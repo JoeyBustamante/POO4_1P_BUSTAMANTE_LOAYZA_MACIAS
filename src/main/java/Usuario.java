bc74c480ff16578386396d73259d315267788525
Joey Bustamante <Joey Bustamante@Joey>
2024-06-19 19:39:49 -0500

    Se creo la clase Usuario con los atributos nombre, apellido y correo, falta de crear el enum Rol y agregarlo a la clase usuario

---
+++ src/main/java/Usuario.java
@@ -0,0 +1,36 @@
+
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String correo;
+
+    
+    //Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+    
+    //Setters
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+}
