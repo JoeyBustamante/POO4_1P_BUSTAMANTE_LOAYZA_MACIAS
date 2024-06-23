1a3de79165994d255896f553f07b9999c238dd58
Joey Bustamante <Joey Bustamante@Joey>
2024-06-22 23:23:47 -0500

    Se creo la clase Interfaz y la clase main

---
+++ src/main/java/Interfaz.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Interfaz {
+    
+    public void inicio(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese A si es Autor, R si es Revisor o E si es editor");
+        String tipo = sc.nextLine();
+        
+        
+    }
+    
+    
+}
