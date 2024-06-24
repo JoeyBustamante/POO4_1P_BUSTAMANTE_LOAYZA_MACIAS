83109245b84f417a49fef01380332c549371e1a9
CjavM <cjavmaci@espol.edu.ec>
2024-06-23 20:04:38 -0500

    Arreglos extras a la clase Mail
    aun no funciona del todo falta el metodo crearSesion

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
-
 public class Main {
     public static void main(String[] args) {
         Interfaz in1 = new Interfaz();
         in1.inicio();
+        //Mail.inicializarSistemaCorreo();
+        //Mail.sendMail("chrismacmen9192@gmail.com","prueba","funciona?");
+        
     }
 }
