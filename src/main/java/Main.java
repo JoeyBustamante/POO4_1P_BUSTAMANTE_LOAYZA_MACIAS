9c68817b9e5e7b82e4a4360ac51d2016d479946a
FrancisLoayza <faloayza@espol.edu.ec>
2024-06-23 20:17:51 -0500

    ewfw

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.Scanner;
 
 import users.Autor;
@@ -7,12 +6,9 @@ import users.Usuario;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-=======
->>>>>>> 83109245b84f417a49fef01380332c549371e1a9
 public class Main {
     Sistema sistema=new Sistema();
     public static void main(String[] args) {
-<<<<<<< HEAD
         Usuario autor=new Autor("Francis", "Loayza", "loayzafrancis@gmail.com", Rol.A, null, 0, null, null, null);
         Sistema.AgergarListaUsuario(autor);
         System.out.println(Sistema.listaUsuario.toString());
@@ -23,12 +19,6 @@ public class Main {
         String usuario=sc.nextLine();
         System.out.println("Ingrese su Contrasena:");
         String contra=sc.nextLine();
-=======
-        Interfaz in1 = new Interfaz();
-        in1.inicio();
-        //Mail.inicializarSistemaCorreo();
-        //Mail.sendMail("chrismacmen9192@gmail.com","prueba","funciona?");
->>>>>>> 83109245b84f417a49fef01380332c549371e1a9
         
     }
 }
