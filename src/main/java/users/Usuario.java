1255a72855f9f5c83f710839a811bf851ba9fd16
FrancisLoayza <faloayza@espol.edu.ec>
2024-06-30 13:48:05 -0500

    p

--- src/main/java/users/Usuario.java
+++ src/main/java/users/Usuario.java
@@ -14,31 +14,14 @@ public class Usuario {
     }
 
     //Getters
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
+    public String getNombre() {return nombre;}
+    public String getApellido() {return apellido;}
+    public String getCorreo() {return correo;}
     
     //Setters
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
+    public void setNombre(String nombre) {this.nombre = nombre;}
+    public void setApellido(String apellido) {this.apellido = apellido;}
+    public void setCorreo(String correo) {this.correo = correo;}
     
     //Metodo toString
     @Override
