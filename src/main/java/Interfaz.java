baa3c1f99760dbd83444f741d4686f8487035f53
FrancisLoayza <faloayza@espol.edu.ec>
2024-06-30 16:41:37 -0500

    Prueba Articulo

--- src/main/java/Interfaz.java
+++ src/main/java/Interfaz.java
@@ -1,9 +1,16 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.Random;
+
+import users.Articulo;
+import users.Autor;
 import users.Editor;
 import users.Revisor;
 import users.Rol;
 import users.Usuario;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
 
 
 public class Interfaz {
@@ -14,10 +21,36 @@ public class Interfaz {
         String tipo = sc.nextLine().toUpperCase();
         switch (tipo) {
             case "1":
-                System.out.println("Autor:");
-                Scanner autor=sc.nextLine();
+                boolean existenteID=true;
+                int artID = (int) (Math.floor(Math.random()*(1000-9999+1)+9999));
+                while (existenteID) {
+                    try {
+                        BufferedReader br=new BufferedReader(new FileReader("src\\main\\java\\Archivos\\Articulos.txt"));
+                        String linea;
+                        while ((linea=br.readLine())!=null) {
+                            String lista[]=linea.split(" ");
+                            int cod=Integer.parseInt(lista[1]);
+                            if(artID!=cod){
+                                existenteID=false;
+                            }
+                            else{
+                                artID=(int) (Math.floor(Math.random()*(1000-9999+1)+9999));
+                            }
+                        }          
+                    } catch (IOException e) {
+                        // TODO: handle exception
+                    }
+                }
+                System.out.println("Nombre:");
+                String nombre=sc.nextLine();
+                System.out.println("Apellido:");
+                String apellido=sc.nextLine();
                 System.out.println("Datos:");
-                Scanner datos=sc.nextLine();
+                String datos=sc.nextLine();
+                System.out.println("Correo:");
+                String correo=sc.nextLine();
+                Autor autor=new Autor(nombre, apellido, correo, Rol.A, 0, null, null, null);
+                Articulo articulo=new Articulo(autor, artID , datos);
 
                 break;
             case "2":
