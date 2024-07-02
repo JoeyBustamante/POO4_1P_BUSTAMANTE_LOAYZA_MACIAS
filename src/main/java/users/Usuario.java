3bd5ba97fdb65cc064bd6d5a4a2b4bae7ef39f72
FrancisLoayza <faloayza@espol.edu.ec>
2024-07-02 17:41:35 -0500

    creacio Equals Usuario

--- src/main/java/users/Usuario.java
+++ src/main/java/users/Usuario.java
@@ -17,11 +17,22 @@ public class Usuario {
     public String getNombre() {return nombre;}
     public String getApellido() {return apellido;}
     public String getCorreo() {return correo;}
+    public Rol getRol(){return rol;}
     
     //Setters
     public void setNombre(String nombre) {this.nombre = nombre;}
     public void setApellido(String apellido) {this.apellido = apellido;}
     public void setCorreo(String correo) {this.correo = correo;}
+    public void setRol(Rol rol){this.rol=rol;}
+
+    public boolean equals(Usuario usuario){
+        if(this.nombre.toUpperCase().equals(nombre.toUpperCase()) && this.apellido.toUpperCase().equals(apellido.toUpperCase()) && this.correo.toUpperCase().equals(correo.toUpperCase()) && this.rol.equals(rol)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     
     //Metodo toString
     @Override
